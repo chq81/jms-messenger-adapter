9c9cfeea04b386c9d8a6366807aaaabbc3b2cb5d
Christoph Quadt <quadt@united-domains.de>
2022-05-09 14:10:00 +0200

    Revert "This is not needed."
    This reverts commit 0465016d1f1ba8d87b667fb9b73cd4babe64abbe.

--- src/Bridge/Symfony/JMSMessengerAdapterBundle.php
+++ src/Bridge/Symfony/JMSMessengerAdapterBundle.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace KunicMarko\JMSMessengerAdapter\Bridge\Symfony;
 
+use KunicMarko\JMSMessengerAdapter\Bridge\Symfony\DependencyInjection\JMSMessengerAdapterExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class JMSMessengerAdapterBundle extends Bundle
 {
+    public function getContainerExtension(): JMSMessengerAdapterExtension
+    {
+        return new JMSMessengerAdapterExtension();
+    }
 }
