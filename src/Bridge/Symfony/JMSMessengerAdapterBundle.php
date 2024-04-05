74176d076ff24398a8472d098d5b29057966ec39
Christoph Quadt <quadt@united-domains.de>
2024-04-05 13:51:37 +0200

    Fixed return values and type-hints.

--- src/Bridge/Symfony/JMSMessengerAdapterBundle.php
+++ src/Bridge/Symfony/JMSMessengerAdapterBundle.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace KunicMarko\JMSMessengerAdapter\Bridge\Symfony;
 
 use KunicMarko\JMSMessengerAdapter\Bridge\Symfony\DependencyInjection\JMSMessengerAdapterExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class JMSMessengerAdapterBundle extends Bundle
 {
-    public function getContainerExtension(): JMSMessengerAdapterExtension
+    /**
+     * @inheritDoc
+     * @return JMSMessengerAdapterExtension|null
+     */
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new JMSMessengerAdapterExtension();
     }
